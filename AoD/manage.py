95687d8cfe3c5d50c2aa79aeb4573235c8804911
Edwin Henneken <ehenneken@gmail.com>
2020-12-22 10:29:02 -0500

    Logging and exception handling updates

--- AoD/manage.py
+++ AoD/manage.py
@@ -5,6 +5,7 @@ from flask_script import Manager, Command, Option
 from app import create_app
 from AoD import generate_batch
 from AoD import post_article
+from utils import post_to_slack
 
 app = create_app()
 # instantiate the manager object
@@ -14,13 +15,36 @@ class GenerateBatch(Command):
 
     def run(self, **kwargs):
         with create_app().app_context():
-            res = generate_batch()
+            resp = generate_batch()
+            # If 'resp' has a key 'Slack' we have to send
+            # a message to Slack
+            if 'Slack' in resp:
+                error_message = {
+                    'text': resp['Slack'],
+                    'link_names': 1
+                }
+                try:
+                    slack = post_to_slack(error_message)
+                except:
+                    current_app.logger.exception("Failed to post to Slack")
 
 class PostArticle(Command):
 
     def run(self, **kwargs):
         with create_app().app_context():
-            res = post_article()
+            resp = post_article()
+            # If 'resp' has a key 'Slack' we have to send
+            # a message to Slack
+            if 'Slack' in resp:
+                error_message = {
+                    'text': resp['Slack'],
+                    'link_names': 1
+                }
+                try:
+                    slack = post_to_slack(error_message)
+                except:
+                    current_app.logger.exception("Failed to post to Slack")
+                
 
 manager.add_command('generate', GenerateBatch())
 manager.add_command('post', PostArticle())
